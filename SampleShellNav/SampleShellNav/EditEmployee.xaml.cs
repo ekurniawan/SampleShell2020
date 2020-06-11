d0c4c88c749f21f6a522dbcaf81b6e505517816e
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-06-11 10:54:12 +0700

    menambahkan edit employee

--- SampleShellNav/SampleShellNav/EditEmployee.xaml.cs
+++ SampleShellNav/SampleShellNav/EditEmployee.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SampleShellNav.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,34 @@ namespace SampleShellNav
         public EditEmployee()
         {
             InitializeComponent();
+        }
+
+        private async void btnUpdate_Clicked(object sender, EventArgs e)
+        {
+            var editData = new Employee
+            {
+                EmpId = Convert.ToInt32(entryEmpID.Text),
+                EmpName = entryEmpName.Text,
+                Designation = entryDesignation.Text,
+                Department = entryDepartment.Text,
+                Qualification = entryQualification.Text,
+                BirthDate = dtBirthdate.Date
+            };
+            try
+            {
+                App.DBUtils.EditEmployee(editData);
+                await DisplayAlert("Keterangan", $"Data berhasil diedit", "OK");
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"{ex.Message}", "OK");
+            }
+        }
+
+        private void btnDelete_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
