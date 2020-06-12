b0c8f496717515545c3f689fc242ae70a59ce3d7
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-06-12 13:42:51 +0700

    menampilkan data employee from services

--- SampleShellNav/SampleShellNav/App.xaml.cs
+++ SampleShellNav/SampleShellNav/App.xaml.cs
@@ -23,7 +23,7 @@ namespace SampleShellNav
             InitializeComponent();
 
             //MainPage = new AppShell();
-            MainPage = new NavigationPage(new ShowEmployee());
+            MainPage = new NavigationPage(new ShowEmployeeServicesPage());
         }
 
         protected override void OnStart()
---
+++ SampleShellNav/SampleShellNav/ShowEmployeeServicesPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SampleShellNav.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleShellNav
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowEmployeeServicesPage : ContentPage
+    {
+        private EmployeeServices _empService;
+        public ShowEmployeeServicesPage()
+        {
+            InitializeComponent();
+            _empService = new EmployeeServices();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            lvEmployee.ItemsSource = await _empService.GetData();
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
